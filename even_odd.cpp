9d2251219f46eaca68b52e6f6f69c4c73d2c59a7
gasser galal <fdxdfc82@gmail.com>
2018-09-14 20:01:02 +0200

    first commit

---
+++ even_odd.cpp
@@ -0,0 +1,15 @@
+#include <iostream> 
+using namespace std ; 
+
+
+int main() 
+{
+
+	int num ; 
+	cout << "enter your num : ";
+	cin >> num ; 
+	if ( num % 2 == 0 ) 
+		cout << " the number is even \n " ;
+	else 
+		cout << " the number is odd \n " ;
+}
